431e58af1e4496e674b87f433a928778873d638c
Chaostradamus <71539742+Chaostradamus@users.noreply.github.com>
2021-09-17 20:26:50 -0400

    fix

--- components/Thumbnail.js
+++ components/Thumbnail.js
@@ -3,7 +3,7 @@ import Image from "next/image";
 import { ThumbUpIcon } from "@heroicons/react/outline";
 import { forwardRef } from "react";
 
-const Thumbnail = forwardRef(({ result }, ref) => { 
+const Thumbnail = forwardRef(({ result }, ref) => {
   const BASE_URL = "https://image.tmdb.org/t/p/original/";
 
   return (
@@ -39,5 +39,7 @@ const Thumbnail = forwardRef(({ result }, ref) => {
     </div>
   );
 });
+
+Thumbnail.displayName = "Thumbnail";
 
 export default Thumbnail;
